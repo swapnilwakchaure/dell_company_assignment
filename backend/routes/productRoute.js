ed1ba8e3c3545c2df704df58dc6258ed4e3ce886
Swapnil Wakchaure <swapnilwakchaure2411999@gmail.com>
2023-07-23 11:10:15 +0530

    backend pagination search route done

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -8,12 +8,16 @@ const productRoute = express.Router();
 // ---------------- GET PRODUCTS IN ONE GET ROUTE ---------------- //
 
 productRoute.get('/', async (request, response) => {
-    const query = request.query;
+    const page = parseInt(request.query._page) || 1;
+    const limit = parseInt(request.query._limit) || 9;
+    const skip = (page - 1) * limit;
+    const query = request.query.q || '';
 
     try {
-        const data = await ProductModel.find(query);
+        const data = await ProductModel.find({ name: query }).skip(skip).limit(limit);
         response.send(data);
     } catch (error) {
+        // console.log('error: ',error);
         response.send(error);
     }
 });
